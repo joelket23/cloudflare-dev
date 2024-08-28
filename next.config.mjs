8c26d5fad33c26088bd89e67a0c8f962ef0ce3a3
joelket23 <kushwahketan11@gmail.com>
2024-08-28 10:09:31 +0530

    as per insturction

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
-
+// console.log('test', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 export default nextConfig;
--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,24 +1,35 @@
+import { getRequestContext } from '@cloudflare/next-on-pages';
 export const config = { runtime: 'edge' };
+
+
 export default async function Authenticate(req, res) {
   const test = { shop: 'staffa' };
 
-  try {
-    const { results } = await process.env.DB.prepare('SELECT * FROM users');
-    // .bind('Joe').all();
-  
-    console.log("resut",results)
-    // return Response.json(results);
-    
-  } catch (error) {
-    console.log("catch error",error.message)
-  }
-  if (test.shop !== 'staff') {
-    console.log('is not staff');
-  }
-  let kvData = await process.env.WORDS.get("joyboy")
-  // kvData = JSON.parse(kvData);
-  console.log("kvdata",kvData)
+  // try {
+  //   const { results } = await process.env.DB.prepare('SELECT * FROM users');
+  //   // .bind('Joe').all();
 
-  return new Response('test', { status: 200 });
+  //   console.log('resut', results);
+  //   // return Response.json(results);
+  // } catch (error) {
+  //   console.log('catch error', error.message);
+  // }
+  // if (test.shop !== 'staff') {
+  //   console.log('is not staff');
+  // }
+  const myVariable = getRequestContext().env.imgz;
+  try {
+    const { env, cf, ctx } = getRequestContext();
+    // const myKv =
+    // let kvData = await process.env.WORDS.get('test');
+    let kvData = await env.words.get("test");
+    // kvData = JSON.parse(kvData);
+    console.log('kvdata', kvData);
+  } catch (error) {
+    console.log("error in kv: ",error.message)
+  }
+ 
+
+  return new Response("test", { status: 200 });
   // return Response.json(results);
 }
